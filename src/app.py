a1040f824696d65847fc43250c92e1aa29c6b499
nayuegg <nayutakahashi1997@gmail.com>
2023-04-12 16:25:53 +0900

    Added a favicon to the pages, and fixed the path to the pretrained weights.

--- src/app.py
+++ src/app.py
@@ -8,20 +8,24 @@ import base64
 import numpy as np
 
 # prediction based on trained model
-#src/model (1).pt
+# src/model (1).pt
+
 
 def predict(img):
     net = Net().cpu().eval()
     # load the parameters of the model
-    net.load_state_dict(torch.load('./model (1).pt', map_location=torch.device('cpu')))
+    net.load_state_dict(torch.load('src/model (1).pt',
+                        map_location=torch.device('cpu')))
     # transform the image
-    img=transform(img)
+    img = transform(img)
     # unsqueeze the image
-    img=img.unsqueeze(0)
+    img = img.unsqueeze(0)
     # predict the class of the image
-    y=torch.argmax(net(img), dim = 1).cpu().detach().numpy()
+    y = torch.argmax(net(img), dim=1).cpu().detach().numpy()
     return y
 # createa a function that returns the name of the given label
+
+
 def label_to_name(label):
     if label == 0:
         return "cat"
@@ -32,47 +36,50 @@ def label_to_name(label):
 
 
 # instantiate the flask app
-app=Flask(__name__)
+app = Flask(__name__)
 
-ALLOWED_EXTENSIONS=set(['png', 'jpg', 'jpeg', "gif"])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', "gif"])
 
 # check if the file is an allowed extension
+
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@ app.route('/', methods = ['GET', 'POST'])
+
+@ app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'filename' not in request.files:
             return redirect(request.url)
         # get data from the file
-        file=request.files['filename']
+        file = request.files['filename']
         # check if the file is an allowed extension
         if file and allowed_file(file.filename):
             # process done to the image file
             # get buffer from the file
-            buf=io.BytesIO()
-            image=Image.open(file)
+            buf = io.BytesIO()
+            image = Image.open(file)
             # write the image to the buffer
-            image.save(buf, format = 'png')
+            image.save(buf, format='png')
             # encode and decode the image
-            base64_str=base64.b64encode(buf.getvalue()).decode('utf-8')
-            base64_data="data:image/png;base64,{}".format(base64_str)
+            base64_str = base64.b64encode(buf.getvalue()).decode('utf-8')
+            base64_data = "data:image/png;base64,{}".format(base64_str)
             # get the prediction
-            prediction=predict(image)
+            prediction = predict(image)
             # get the name of the prediction
-            name=label_to_name(prediction)
+            name = label_to_name(prediction)
             # return the prediction
-            return render_template('result.html', animalName = name, image = base64_data)
+            return render_template('result.html', animalName=name, image=base64_data)
 
         return redirect(request.url)
-
 
     elif request.method == 'GET':
         return render_template('index.html')
 
+
 # run the application
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
